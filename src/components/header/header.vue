5505ad5d5ba5b12d96414ace6f370266714b53e6
Simon <ssmdmsn@hotmail.com>
2017-03-17 17:42:48 +0800

    vue demo source.

---
+++ src/components/header/header.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="header">
+    I am header.
+  </div>
+</template>
+
+<script type="ecmascript-6">
+  new
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+
+</style>
